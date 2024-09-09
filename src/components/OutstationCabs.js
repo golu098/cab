7b5ca39f03626a3648c2e42332063648041cdf0b
shivkant-epam <shivkantkr9934@gmail.com>
2024-09-09 15:00:42 +0530

    Revert "Header file"
    This reverts commit cb9a0ca40db043c5e0c63a6ca6a6490a946e58e2.

--- src/components/OutstationCabs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function OutstationCabs() {
-  return (
-    <div>OutstationCabs</div>
-  )
-}
--- src/components/TourPackages.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TourPackages() {
-  return (
-    <div>TourPackages</div>
-  )
-}
